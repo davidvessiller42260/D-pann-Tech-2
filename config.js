a9e4f3e08e1d9ce5cb00f2d4f6424cdd67211537
davidvessiller42260 <david.vessiller42260@gmail.com>
2026-09-20 10:20:27 +0200

    Update config.js
    adresse mail ou doit etre envoyer les devis

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Configuration DÉPANN’TECH pour GitHub Pages
 // Modifiez uniquement cette adresse si vous souhaitez recevoir les demandes de devis.
 window.DEPANNTECH_CONFIG = {
-  email: ""
+  email: "david.vessiller42260@gmail.com"
 };
