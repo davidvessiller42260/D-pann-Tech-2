ce2c345ea261ccf6cf1a506d31b113b83e19bf12
davidvessiller42260 <david.vessiller42260@gmail.com>
2026-09-20 10:23:16 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Configuration DÉPANN’TECH pour GitHub Pages
-// Modifiez uniquement cette adresse si vous souhaitez recevoir les demandes de devis.
-window.DEPANNTECH_CONFIG = {
-  email: "david.vessiller42260@gmail.com"
-};
